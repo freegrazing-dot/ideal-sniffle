c7a2397f1b668df0cab3b21f43230356d1a45961
Mark Shrode <tkacvacations@gmail.com>
2026-04-06 20:28:35 -0600

    Add horizontal scroll to admin tabs for Merchandise access

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 import { LoginForm } from '../components/auth/LoginForm'
 import { SignupForm } from '../components/auth/SignupForm'
 
